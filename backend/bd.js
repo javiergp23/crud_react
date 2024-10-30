bd5545498134e59eedbf409e1455adefb92a2538
Javier Gonzalez <javgon2306@gmail.com>
2024-10-30 09:06:34 -0300

    changes

--- backend/bd.js
+++ backend/bd.js
@@ -1,10 +1,8 @@
 import mysql from 'mysql';
-//hay que alterar la conexion a la base de datos 
 
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    //agregar el nuevo password a la base de datos mysql
     password: "",
     database: "crud2"
 });
